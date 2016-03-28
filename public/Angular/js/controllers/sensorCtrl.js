947cda9b2d7cc364ebe6750ba92e80b3005c50ac
dotam <dothanhtam9999@gmail.com>
2016-03-28 15:09:44 +0700

    Update sensorCtrl.js

--- public/Angular/js/controllers/sensorCtrl.js
+++ public/Angular/js/controllers/sensorCtrl.js
@@ -1,4 +1,4 @@
-var host = 'http://app3-demo.openshift.rasia/';
+var host = 'http://app3-demo.openshift.rasia';
 
 app.controller('sensorCtrl', ['$scope', '$rootScope', '$http', 'DTOptionsBuilder', 'DTColumnBuilder', '$compile',
     function($scope, $rootScope, $http, DTOptionsBuilder, DTColumnBuilder, $compile) {
